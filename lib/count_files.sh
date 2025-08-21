e7e3abb3c0eb678567e5f14a9feae9839111cd40
Jonnie <jonniebarnsley@gmail.com>
2025-08-21 19:15:05 +0100

    deleted now redundant scripts

--- lib/count_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# function to count the number of files in a directory which match a given pattern.
-count_files() {
-    local directory="$1"
-    local pattern="$2"
-    find "$directory" -maxdepth 1 -type f -name "$pattern" | wc -l
-}
--- lib/ping.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-message=$1
-message=${message:=Ping!}
-discord_url=https://discord.com/api/webhooks/1334194162991370391/1rJ6t6WqLH8QXEpJzzfyp3kKZ69EQQ7ZJUPoG-H0b3UJ1R8RRPjcBYLVIAyA59dTGtB3
-
-curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$message\"}" $discord_url
-
