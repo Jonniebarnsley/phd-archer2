c7982f8392c6f0f5b3f5739236a37ec9edb4f513
Jonnie <jonniebarnsley@gmail.com>
2025-12-10 12:53:45 +0000

    import BisiclesFile from separate script

--- lib/process_plot.py
+++ lib/process_plot.py
@@ -3,36 +3,34 @@
 """
 Script to extract data from hdf5 plotfiles and save it as a netcdf.
 
-Usage: "python process_plot.py <variable> <plot_directory> [options]
+Usage: "python process_plot.py <plot_directory> <outfile_path> <variable> [<variable> ...] [options]
 Options:
- -d     : output_directory
- -f     : output_file
- --lev  : level of refinement
-
-Use with either -d or -f, not both. If -d, the output will be sorted into automatically generated
-subdirectories for level of refinement and variable. -f allows users to specify a custom filepath,
-overriding anything defined by -d.
+ --lev   : level of refinement
+ --order : interpolation order (0=piecewise constant, 1=linear)
 """
 
 import argparse
+import numpy as np
 import xarray as xr
 from pathlib import Path
 from mpi4py import MPI # needed to run the MPI routines in amrio on archer2
 
-# NB: amrfile needs the BISICLES AMRfile directory added to PYTHONPATH and the libamrfile directory
-# added to LD_LIBRARY_PATH – see my .bashrc for an example
-from bisiclesio import BisiclesFile
+# NB: amrfile and, by extension, BisiclesFile need the BISICLES AMRfile directory added to PYTHONPATH 
+# and the libamrfile directory added to LD_LIBRARY_PATH – see my .bashrc for an example
+from bisiclesfile import BisiclesFile
 
 class Processor:
 
     def __init__(
         self, 
-        variable: str, 
+        variables: list, 
         lev: int=0,
+        order: int=0
     ):
-        self.variable = variable
+        self.variables = variables
         self.lev = lev
-    
+        self.order = order
+
     @property
     def encoding_specs(self):
         """Encoding specifications for netcdf output"""
@@ -40,7 +38,7 @@ class Processor:
         tchunks = 147 if self.lev == 0 else 49  # 2 time chunks for 0lev when simulating 2007-2300
         self.specs = {
             'zlib': True,
-            'complevel': 3,
+            'complevel': 4,
             'dtype': 'int32',
             'scale_factor': 0.001,   # 3 decimal places precision
             '_FillValue': -9999,
@@ -56,10 +54,17 @@ class Processor:
             print(f"({i}/{len(files)}) {file.name}")
 
             with BisiclesFile(file) as bfile:
-                ds = bfile.read_dataset(self.variable, lev=self.lev)
+                ds = bfile.read_dataset(self.variables, lev=self.lev, order=self.order)
                 time = bfile.query_time()
+
+            # Sometimes, multiple plotfiles can be written with times e.g. 200.0000 and 200.0001.
+            # We skip files with times that are very close to the last time in the batched dataset.
+            if batched is not None and np.isclose(batched.time.values[-1], time, atol=0.5):
+                print(f"Time {time} already exists in dataset. Skipping file {file.name}.")
+                continue
+
             ds = ds.expand_dims('time')
-            ds = ds.assign_coords(time=[time])
+            ds = ds.assign_coords(time=[round(time, 0)])
 
             if batched is None:
                 batched = ds
@@ -81,9 +86,10 @@ class Processor:
             print(f"No plot files found in {plot_dir}")
             return
         
-        print(f"Processing {self.variable} at level {self.lev} from {plot_dir}")
+        print(f"Processing variables: {', '.join(self.variables)} at level {self.lev} from {plot_dir}")
         ds = self.batch_time(files)
-        ds[self.variable].encoding.update(self.encoding_specs)
+        for var in ds.data_vars:
+            ds[var].encoding.update(self.encoding_specs)
 
         print(f"Generating {outfile}...")
         try:
@@ -94,21 +100,7 @@ class Processor:
             outfile.unlink()
         except KeyboardInterrupt:
             outfile.unlink()
-
-def auto_filepath(ctrl_dir: Path, savedir: Path, variable: str, lev: int) -> Path:
-    """
-    Generates a filepath for the processed netcdf following the directory structure:
-    <savedir>/<lev>lev/<variable>/<ensemble>_<run>_<variable>_<lev>lev.nc
-    """
-
-    ctrl_dir = ctrl_dir.resolve()
-    run = ctrl_dir.parent
-    ensemble = run.parent
-
-    auto_dir = savedir / f'{lev}lev' / variable 
-    auto_path = auto_dir / f'{ensemble.name}_{run.name}_{variable}_{lev}lev.nc'
-
-    return auto_path
+        print('done')
 
 def create_parser() -> argparse.ArgumentParser:
 
@@ -116,13 +108,13 @@ def create_parser() -> argparse.ArgumentParser:
         description="Process BISICLES plot files into a netcdf format")
 
     # add arguments
-    parser.add_argument("variable", type=str, help="variable name") 
-    parser.add_argument("ctrl", type=Path, help="Path to BISICLES output directory")
+    parser.add_argument("plot_dir", type=Path, help="Path to BISICLES plot directory")
+    parser.add_argument("outfile", type=Path, help="Filepath for output netcdf file")
+    parser.add_argument("variables", type=str, nargs='+', help="One or more variable names") 
 
     # add optional arguments
     parser.add_argument("--lev", type=int, default=0, help="level of refinement")
-    parser.add_argument("-d", "--dir", type=Path, help="Directory to save netcdf files")
-    parser.add_argument("-f", "--file", type=Path, help="Filepath for output netcdf file")
+    parser.add_argument("--order", type=int, default=0, help="interpolation order (0=piecewise constant, 1=linear)")
 
     return parser
 
@@ -130,21 +122,9 @@ def main():
 
     parser = create_parser()
     args = parser.parse_args()
-
-    if args.file:
-        outfile_path = args.file
-        absolute_path = outfile_path.resolve()
-        absolute_path.parent.mkdir(parents=True, exist_ok=True)
-    elif args.dir:
-        outfile_path = auto_filepath(args.ctrl, args.dir, args.variable, args.lev)
-        absolute_path = outfile_path.resolve()
-        absolute_path.parent.mkdir(parents=True, exist_ok=True)
-    else:
-        print("Must either specify a directory (-d) or filepath (-f) for output")
-        return
-
-    proc = Processor(variable=args.variable, lev=args.lev)
-    proc.process_ctrl(args.ctrl, outfile_path)
+    args.outfile.parent.mkdir(parents=True, exist_ok=True)
+    proc = Processor(variables=args.variables, lev=args.lev)
+    proc.process_plot(args.plot_dir, args.outfile)
 
 if __name__ == "__main__":
     main()
