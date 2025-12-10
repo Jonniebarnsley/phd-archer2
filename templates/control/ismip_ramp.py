74c10d945f1572a004035cbd522df23031138c86
Jonnie <jonniebarnsley@gmail.com>
2025-12-10 13:09:54 +0000

    deleted some old templates

--- templates/control/ismip_ramp.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def ramp(x,y,t,*etc):
-    
-    coef = 1.0
-    if (t < 100.0):
-        coef = math.floor(t) / 100.0
-
-    return coef # originally in Stephs ismip was return -coef
--- templates/control/surface_temp_module.py
+++
@@ -1,2 +0,0 @@
-def surface_temperature(x,y,t,thck,topg):
-    return 268 - ((thck + topg) * 0.005)
